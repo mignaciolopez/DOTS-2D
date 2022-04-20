a358651a8736754022c5d5b82e0e4a0338fc8e88
lmartinignacio <lmartinignacio@gmail.com>
2022-04-20 19:17:18 -0300

    Playing with lights

---
+++ Assets/Scripts/Graphics/GameGraphicsDots.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGraphicsDots : MonoBehaviour
+{
+    [SerializeField] Mesh mesh;
+    [SerializeField] Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
